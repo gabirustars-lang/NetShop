3b18e6a64bc6a84221902260a21a6083a9fcfec9
gabirustars-lang <gabiru.stars@gmail.com>
2026-03-25 20:04:17 -0300

    uptade 0.1.1

--- script.js
+++ script.js
@@ -380,17 +380,4 @@ function filtrar() {
     });
 }
   
-});
-
-function search() {
-  let input = document.getElementById('Barra de Pesquisa').value
-  input = input.toLowerCase();
-  let x = document.getElementsByClassName('Pesquisar PCs')
-
-  for(i = 0; i < x.length; i++){
-    if(!x[i].innerHTML.toLowerCase().includes(input))
-      x[i].style.display = "none"
-    else
-      x[i].style.display = "list-item"
-  }
-}
+})
